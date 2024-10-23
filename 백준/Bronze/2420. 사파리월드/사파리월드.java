718ab06fa213c6c3dde7fb6c62881f4d3641464f
wpcks99 <120312941+wpcks99@users.noreply.github.com>
2024-10-23 16:34:26 +0900

    [Bronze V] Title: 사파리월드, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextInt();
+        long b = scanner.nextInt();
+
+        System.out.print(Math.abs(a-b));
+
+        }
+    }
