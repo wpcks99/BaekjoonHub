4006e183b400bf1264b7c19b7b496b35d68a3052
wpcks99 <120312941+wpcks99@users.noreply.github.com>
2024-10-28 21:19:54 +0900

    [Bronze V] Title: 이상한 기호, Time: 108 ms, Memory: 14132 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -7,10 +7,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-
-        System.out.println((A + B) * (A - B));
+        
+        long result = (long) (A + B) * (A - B);
+        
+        System.out.println(result);
     }
 }
