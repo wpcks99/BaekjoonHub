2f11c1ae96ef2933bcdb7bad309fc7efbb198935
wpcks99 <120312941+wpcks99@users.noreply.github.com>
2024-10-28 21:11:55 +0900

    [Bronze III] Title: 학점계산, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        HashMap<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.3);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("A-", 3.7);
+        gradeMap.put("B+", 3.3);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("B-", 2.7);
+        gradeMap.put("C+", 2.3);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("C-", 1.7);
+        gradeMap.put("D+", 1.3);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("D-", 0.7);
+        gradeMap.put("F", 0.0);
+        
+        String grade = br.readLine();
+        System.out.println(gradeMap.get(grade));
+    }
+}
